f93c4f5e9bfdfb0bf25dc9d225ac19ad5d286e19
Flynn Stacey <flynnstacey@hotmail.com>
2023-12-01 08:41:35 +0000

    changed file name for interfaces

--- src/interfaces/taskInterfaces.ts
+++ src/interfaces/taskInterfaces.ts
@@ -5,3 +5,10 @@ export interface Task {
   isCompleted: boolean;
   completed_at: Date | null;
 }
+
+export interface TaskModalProps {
+  task?: Task | null;
+  onClose: () => void;
+  refetch: () => void;
+  isModalOpen: boolean;
+}
